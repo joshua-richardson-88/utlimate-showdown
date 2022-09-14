5c2774d2a225e17a0dd21d0a4224fdfd8ba8a428
Joshua Richardson <josh@cursedtale.com>
2022-09-13 17:36:52 -0700

    fix(character): remove unneeded map on db

--- src/server/router/characters.ts
+++ src/server/router/characters.ts
@@ -2,8 +2,8 @@ import { TRPCError } from '@trpc/server'
 import { createRouter } from './context'
 
 export const characterRouter = createRouter().query('getAll', {
-  resolve: async ({ ctx }) => {
-    const dbChars = await ctx.prisma.character.findMany({
+  resolve: async ({ ctx }) =>
+    await ctx.prisma.character.findMany({
       select: {
         id: true,
         name: true,
@@ -14,14 +14,5 @@ export const characterRouter = createRouter().query('getAll', {
         bg_color: true,
       },
       orderBy: { name: 'asc' },
-    })
-    if (dbChars == null) return []
-
-    return dbChars.map((char) => ({
-      ...char,
-      icon: `bg-[url(${char.icon})]`,
-      bg_border: `border-${char.bg_border}`,
-      bg_color: `bg-${char.bg_color}`,
-    }))
-  },
+    }),
 })
